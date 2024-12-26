8f8491accafb8fe463c332f1d4c9e56d62fc135b
Park <pastly500@naver.com>
2024-12-27 00:17:51 +0900

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//통합 개발 환경(IDE) : Integrated Development Environment = 프로그램 개발과 관련한 모든 작업을 처리할 수 있는 환경을 제공하는 도구
+
+int main() {
+	printf("Hello World \n");
+
+	//\n : 줄바꿈 하라는 의미
+	//\t : 탭 기능 의미
+}
